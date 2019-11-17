c717750d919cadf535d25027e076ce835944fe8a
skguru <55821282+skguru@users.noreply.github.com>
2019-11-17 13:00:48 -0800

    updates

--- queries/schema.sql
+++ queries/schema.sql
@@ -12,6 +12,8 @@ CREATE TABLE employees (emp_no INT NOT NULL,
      hire_date DATE NOT NULL,
      PRIMARY KEY (emp_no));
 
+drop table dept_manager ;
+
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
@@ -31,5 +33,22 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+drop table dept_emp;
 
-select * from departments;
+create table dept_emp(emp_no INT NOT NULL,
+					 dept_no VARCHAR(4) NOT NULL,
+					 from_date DATE NOT NULL,
+					 to_date DATE NOT NULL,
+					 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+					 PRIMARY KEY (emp_no, dept_no));
+
+
+drop table titles;
+
+create table titles(emp_no INT NOT NULL,
+				   title VARCHAR(40) NOT NULL,
+				   from_date DATE NOT NULL,
+				   to_date DATE NOT NULL,
+					FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+					);
+
